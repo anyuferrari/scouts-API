c4dd5d68bd28bf74ca65e03713249403228d8e54
Mauricio Ferrari <anyuferrari@gmail.com>
2022-03-06 18:52:15 -0300

    Initial commit

---
+++ src/main/java/com/scouts/scouts/ScoutsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.scouts.scouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutsDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutsDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scouts/scouts/ScoutsDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scouts.scouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutsDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
